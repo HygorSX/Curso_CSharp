9ad959ba7f5df00d82a781eaed00d6d0ef94370f
HygorSX <116827177+HygorSX@users.noreply.github.com>
2023-08-01 21:48:47 -0300

    Update Ex_01.cs

--- Exercícios/Ex_01.cs
+++ Exercícios/Ex_01.cs
@@ -1,1 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um número");
+            float num_usuario = float.Parse(Console.ReadLine());
+
+            if (num_usuario == 0)
+            {
+                Console.WriteLine("Seu número é igual a zero");
+            } else if(num_usuario > 0)  
+            {
+                Console.WriteLine("Seu número é Positivo");
+            }else
+            {
+                Console.WriteLine("Seu número é negativo");
+            }
+            Console.ReadLine();
+        }
+    }
+}
 

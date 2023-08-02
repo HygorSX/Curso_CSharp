5c7726a038ea663234e95b9d984d9eafbebed1b9
HygorSX <116827177+HygorSX@users.noreply.github.com>
2023-08-01 21:50:27 -0300

    Update and rename Ex_04.cs to Ex_03.cs

---
+++ Exercícios/Ex_03.cs
@@ -0,0 +1,34 @@
+            using System;
+            using System.Collections.Generic;
+            using System.Linq;
+            using System.Text;
+            using System.Threading.Tasks;
+
+namespace Ex_1
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                Console.WriteLine("Qual o seu salário?");
+                float sal = float.Parse(Console.ReadLine());
+
+                if (sal <= 300)
+                {
+                    float salReajuste = sal * 0.5f;
+                    float salReajustado = sal - salReajuste;
+                    Console.WriteLine("O seu salário reajustado é: " +salReajustado);
+
+                }
+                else
+                {
+                    float salReajuste = sal * 0.3f;
+                    float salReajustado = sal - salReajuste;
+                    Console.WriteLine("O seu salário reajustado é: " + salReajustado);
+
+            }
+            
+                Console.ReadLine();
+            }
+        }
+    }

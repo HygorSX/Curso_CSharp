078346a4218a0c7da134dec68de33b417ef89e61
HygorSX <116827177+HygorSX@users.noreply.github.com>
2023-08-03 21:44:15 -0300

    Update EditorTXT.cs

--- Projetos/EditorTXT.cs
+++ Projetos/EditorTXT.cs
@@ -1,58 +1,87 @@
-// See https://aka.ms/new-console-template for more information
+using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-static void Menu()
+namespace EditorTXT
 {
-    Console.Clear();
-    Console.WriteLine("O que você deseja fazer?");
-    Console.WriteLine("1 - Abrir arquivo");
-    Console.WriteLine("2 - Criar novo arquivo");
-    Console.WriteLine("0 - Sair");
-    short option = short.Parse(Console.ReadLine());
-
-    switch (option)
+    internal class Program
     {
-        case 0: System.Environment.Exit(0); break;
-        case 1: Abrir(); break;
-        case 2: Editar(); break;
-        default: Menu(); break;
+        static void Main(string[] args)
+        {
+            Menu();
+        }
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("O que você deseja fazer?");
+            Console.WriteLine("1 - Abrir arquivo");
+            Console.WriteLine("2 - Criar novo arquivo");
+            Console.WriteLine("0 - Sair");
+            short option = short.Parse(Console.ReadLine());
+
+            switch (option)
+            {
+                case 0: System.Environment.Exit(0); break;
+                case 1: Abrir(); break;
+                case 2: Editar(); break;
+                default: Menu(); break;
+            }
+        }
+
+        static void Abrir()
+        {
+            Console.Clear();
+            Console.WriteLine("Qual caminho do arquivo?");
+            string path = Console.ReadLine();
+
+            using(var file = new StreamReader(path))
+            {
+                string text = file.ReadToEnd();
+                Console.WriteLine(text);
+            }
+            Console.WriteLine("");
+            Console.ReadLine();
+            Menu();
+        }
+
+        static void Editar()
+        {
+            Console.Clear();
+            Console.WriteLine("Digite seu texto abaixo (ESC para sair)");
+            Console.WriteLine("_______________________________________");
+            string text = "";
+
+
+            do
+            {
+                text += Console.ReadLine();
+                text += Environment.NewLine;
+                //quebrando a linha no fim de cada leitura
+
+            }
+            while (Console.ReadKey().Key != ConsoleKey.Escape);
+            //enquanto o usuário não apertar ESC, ele não poderá sair
+            Salvar(text);
+        }
+
+        static void Salvar(string text)
+        {
+            Console.Clear();
+            Console.WriteLine("qual caminho para salvar o arquivo?");
+            var path = Console.ReadLine();
+
+            using (var file = new StreamWriter(path))
+            {
+                file.Write(text);
+            }
+
+            Console.WriteLine($"Arquivo {path} salvo com sucesso!");
+            Console.ReadLine();
+            Menu();
+
+        }
     }
-}
-
-static void Abrir()
-{
-
-}
-
-static void Editar()
-{
-    Console.Clear();
-    Console.WriteLine("Digite seu texto abaixo (ESC para sair)");
-    Console.WriteLine("_______________________________________");
-    string text = "";
-
-
-    do
-    {
-        text += Console.ReadLine();
-        text += Environment.NewLine;
-        //quebrando a linha no fim de cada leitura
-
-    }
-    while (Console.ReadKey().Key != ConsoleKey.Escape);
-    //enquanto o usuário não apertar ESC, ele não poderá sair
-    Console.WriteLine(text);
-}
-
-static void Salvar(string text)
-{
-    Console.Clear();
-    Console.WriteLine("qual caminho para salvar o arquivo?");
-    var path = Console.ReadLine();
-
-    using (var file = new StreamWriter(path))
-    {
-        file.Write(text);
-    }
-
 }

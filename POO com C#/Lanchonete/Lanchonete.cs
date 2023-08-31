51913b896d300e4333c3b30761a327a650f1b274
HygorSX <116827177+HygorSX@users.noreply.github.com>
2023-08-31 15:13:51 -0300

    Create Lanchonete.cs

---
+++ POO com C#/Lanchonete/Lanchonete.cs
@@ -0,0 +1,55 @@
+using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        public class Lanchonete
+        {
+            public string nomeCliente { get; set; }
+            public double taxaDeServico { get; set; }
+            public string molhoPizza { get; set; }
+            public string recheioPizza { get; set; }
+            public string bordaPizza { get; set; }
+            public string paoLanche { get; set; }
+            public string recheioLanche { get; set; }
+            public string molhoLanche { get; set; }
+            public string recheioSalgadinho { get; set; }
+            public string massaSalgadinho { get; set; }
+
+            public Lanchonete(string nomeCliente, double taxaDeServico, string molhoPizza, string recheioPizza, string bordaPizza, string paoLanche, string recheioLanche, MO
+            {
+              
+            }
+
+
+                public void Pizza()
+                {
+
+                }
+                public void Lanche()
+                {
+
+                }
+                public void Salgadinho()
+                {
+
+                }
+                public void Prato()
+                {
+
+                }
+                public void Pedido()
+                {
+
+                }
+            }
+        
+
+            static void Main(string[] args)
+            {
+
+            }
+        }
+    }
+

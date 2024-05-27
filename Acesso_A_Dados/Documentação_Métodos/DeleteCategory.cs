a008ae3ffbc23180c8ccd76df907edf82a0b7356
HygorSX <116827177+HygorSX@users.noreply.github.com>
2024-05-27 10:58:00 -0300

    Create DeleteCategory.cs

---
+++ Acesso_A_Dados/Documentação_Métodos/DeleteCategory.cs
@@ -0,0 +1,18 @@
+// Define um método estático chamado DeleteCategory que recebe uma conexão com o banco de dados como parâmetro
+static void DeleteCategory(SqlConnection connection)
+{
+    // Define a consulta SQL para deletar uma categoria específica na tabela 'Category'
+    var deleteCategory = 
+        @"DELETE FROM [Category] 
+        WHERE [Id]=@Id";
+
+    // Executa a consulta SQL usando a conexão com o banco de dados
+    // Passa o valor necessário para a consulta como parâmetro
+    var rows = connection.Execute(deleteCategory, new
+    {
+        id = "12F88683-0FF5-4916-BBF3-85655B9BF82D",  // Define o Id da categoria que será deletada
+    });
+
+    // Exibe no console o número de registros que foram deletados no banco de dados
+    Console.WriteLine($"{rows} registros deletados!");
+}

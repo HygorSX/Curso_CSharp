38122740a946b820e41ef7ab348859f2322e4d45
HygorSX <116827177+HygorSX@users.noreply.github.com>
2024-05-27 09:27:31 -0300

    Create Course.cs

---
+++ Acesso_A_Dados/Course.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcessoAosDados
+{
+    public class Course
+    {
+        public Guid Id { get; set; }
+        public String Title { get; set; }
+    }
+}
